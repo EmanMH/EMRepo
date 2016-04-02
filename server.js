a13d1ba68de5ef4cb3708f092f15d4db718199f3
emahmoud <eman.mahmoud@armaturecorp.com>
2016-04-02 18:12:52 +0200

    Node Server update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var easyrtcServer = easyrtc.listen(httpApp, socketServer);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://bbtodesksandbox.azurewebsites.net');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
